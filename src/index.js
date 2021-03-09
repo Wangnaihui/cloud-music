0e09493706debf0f900a871f0bd413d415b08a00
wangnaihui <qinzhen.2021@bytedance.com>
2021-03-09 10:41:34 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'   //导入项目的样式
+import App from './App'
+
+ReactDOM.render(
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
+  document.getElementById('root')
+)
+
